4ae1e3c07de2e73212cff251a01375494d30e5e4
Edmundas Matusevičius <edmundasm@gmail.com>
2022-11-05 14:44:33 +0200

    Initial project.

---
+++ Posts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Edmundas Matusevičius on 2022-11-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
